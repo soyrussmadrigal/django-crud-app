fc97bca45842fec4d36abe3b10b3ae38018c1d56
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-01-22 16:58:57 -0600

    test

--- PythonApp/tasks/views.py
+++ PythonApp/tasks/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def helloworld(request):
-    return HttpResponse("Hello World from Djangossssss!")
+    return HttpResponse("Hello World from Djangossssssssssss!")
