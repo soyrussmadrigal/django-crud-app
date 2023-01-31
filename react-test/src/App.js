72867645cf62727a6bd83ec93c022d5ea9b7f2d2
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-01-31 10:36:00 -0600

    Update

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test <code>src/App.js</code> and save to reload.
+          sssss <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
