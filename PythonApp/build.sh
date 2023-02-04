f0bf69fb0ce623580bdc45d46569aff94926cff5
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-04 10:31:58 -0600

    Update

--- PythonApp/build.sh
+++ PythonApp/build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-# poetry install
-pip3 install -r requirements.txt
+#poetry install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
--- PythonApp/djangoapp/settings.py
+++ PythonApp/djangoapp/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'djangoapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     )
-    
 }
 
 
@@ -122,8 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# This setting tells Django at which URL static files are going to be served to the user.
+# Here, they well be accessible at your-domain.onrender.com/static/...
+STATIC_URL = '/static/'
+# Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
